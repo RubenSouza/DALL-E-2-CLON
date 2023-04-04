4a464d13651e0ab8816c1e64f8f20cb2d10c1497
RubenSouza <rubem.eliel20212@gmail.com>
2023-04-04 16:28:57 -0300

    next config cache control

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: ["links.papareact.com", "firebasestorage.googleapis.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
